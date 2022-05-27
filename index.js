753e14fb6d1a9df77b062d44fd3a72b33abb1a2f
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-05-27 15:26:24 +0100

    Completed the calculations and literal templates

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let mealCost = 25.99;
-let tip = 0.18;
-let tipAmount = Number(mealCost * tip);
+let mealCost = Number (prompt("How much did the meal cost? ex 25.99"));
+let tip = Number (prompt("How much is the tip supposed to be? ex 0.18 for 18%"));
+let tipAmount = mealCost * tip;
 console.log(tipAmount.toFixed(2));
 
 let total = mealCost + tipAmount;
+
 console.log(total.toFixed(2));
 
-let numOfPeople = 2;
+let numOfPeople = prompt("The number of people dining are?")
 
-console.log(`Your meal cost is $${mealCost}, and with the tip it comes to ${total.toFixed(2)}. Your share is $${total.toFixed(1)/numOfPeople}. Hope it was delicious!`)
+console.log(`Your meal cost is $${mealCost}, and with the tip it comes to ${total.toFixed(2)}. Your share is $${(total/numOfPeople).toFixed(2)}. Hope it was delicious!`)
